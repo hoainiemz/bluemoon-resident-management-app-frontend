0f5befb5577dc02216f8ca9f435fb32cae10619d
hoainiemz <nam.leo1805@gmail.com>
2025-05-11 22:31:41 +0700

    f

--- src/main/java/org/example/hellofx/AppConfig.java
+++ src/main/java/org/example/hellofx/AppConfig.java
@@ -7,7 +7,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
-    public static String backendUrl = "https://bluemoon-resident-management-app-backend.onrender.com/";
+//    public static String backendUrl = "https://bluemoon-resident-management-app-backend.onrender.com/";
+    public static String backendUrl = "https://hoainiem-bluemoon-106512668344.asia-east1.run.app";
 
     @Bean
     public RestTemplate restTemplate() {
